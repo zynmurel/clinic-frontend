70a8c0d5fa8bc6e5d5494889604d0260a00b9270
Sean Maruel Anub Comingues <maui@Seans-Mac-mini.local>
2024-10-31 17:28:46 +0800

    another ajustment

--- src/app/(auth)/login/layout.tsx
+++ src/app/(auth)/login/layout.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Layout = ({ children }: { children: React.ReactElement }) => {
     return (
         <div className="bg-gradient-to-r from-slate-100 to-slate-200 min-h-screen relative flex items-center justify-center p-2 sm:p-5 overflow-hidden">
-            <Image src="/svg/rose.svg" alt="Icon" width={1000} height={1000} className=" absolute -left-32 md:-left-72 -bottom-0 md:-bottom-60 opacity-60" />
+            <Image src="/svg/rose.svg" alt="Icon" width={1000} height={1000} className=" absolute -left-32 md:-left-72 -bottom-0 md:-bottom-60 opacity-40" />
             {children}
         </div>
     );
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -16,14 +16,14 @@ const visits = [{
 
 const Page = async () => {
     return (
-        <div className=" lg:w-full max-w-5xl md:grid-cols-2 grid p-5  lg:grid-cols-5 rounded-2xl shadow bg-white z-10">
+        <div className="bg-gradient-to-r from-amber-50 to-white lg:w-full max-w-5xl md:grid-cols-2 grid p-5  lg:grid-cols-5 rounded-2xl shadow bg-white z-10">
             <div className=" lg:col-span-3 hidden md:flex flex-col p-10 justify-center items-center gap-2 rounded-lg">
                 <Image
-                    src="/images/layaw-logo.png"
+                    src="/images/layaw_logo.png"
                     alt="Descriptive Alt Text"
                     width={350}
                     height={250}
-                    className=" object-cover w-full h-auto lg:px-16  bg-white"
+                    className=" object-cover w-full h-auto lg:px-16 "
                 />
                 <p className=" text-sm text-center font-semibold text-primary uppercase opacity-80">
                     Medical Spa · Laser Hair Removal Service · Beauty, cosmetic & personal care
@@ -32,9 +32,9 @@ const Page = async () => {
                     {visits.map((vis, index) => (<Link href={vis.url} key={index} target="_blank"><div className=" p-2 rounded-full bg-amber-100 text-primary hover:brightness-95">{vis.icon}</div></Link>))}
                 </div>
             </div>
-            <div className=" lg:col-span-2 bg-white flex flex-col items-center justify-center">
+            <div className=" lg:col-span-2 flex flex-col items-center justify-center">
                 <Image
-                    src="/images/layaw-logo.png"
+                    src="/images/layaw_logo.png"
                     alt="Descriptive Alt Text"
                     width={200}
                     height={250}
