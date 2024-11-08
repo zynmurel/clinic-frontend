0a85557c518efacddf8fd32ec9933503094fe38e
Sean Maruel Anub Comingues <maui@Seans-Mac-mini.local>
2024-11-08 17:23:27 +0800

    changes

--- src/app/(auth)/login/layout.tsx
+++ src/app/(auth)/login/layout.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const Layout = ({ children }: { children: React.ReactElement }) => {
     return (
-        <div className="bg-gradient-to-r from-slate-100 to-slate-200 min-h-screen relative flex items-center justify-center p-2 sm:p-5 overflow-hidden">
-            <Image src="/svg/rose.svg" alt="Icon" width={1000} height={1000} className=" absolute -left-32 md:-left-72 -bottom-0 md:-bottom-60 opacity-40" />
+        <div className="bg-gradient-to-r from-slate-100 to-slate-300 min-h-screen relative flex items-center justify-center p-2 sm:p-5 overflow-hidden">
+            {/* <Image src="/svg/rose.svg" alt="Icon" width={1000} height={1000} className=" absolute -left-32 md:-left-72 -bottom-0 md:-bottom-60 opacity-50" /> */}
             {children}
         </div>
     );
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -16,14 +16,14 @@ const visits = [{
 
 const Page = async () => {
     return (
-        <div className="bg-gradient-to-r from-amber-50 to-white lg:w-full max-w-5xl md:grid-cols-2 grid p-5  lg:grid-cols-5 rounded-2xl shadow bg-white z-10">
-            <div className=" lg:col-span-3 hidden md:flex flex-col p-10 justify-center items-center gap-2 rounded-lg">
+        <div className="lg:w-full max-w-6xl md:grid-cols-2 grid py-10  lg:grid-cols-5 rounded overflow-hidden shadow bg-white z-10">
+            <div className=" lg:col-span-3 hidden md:flex px-10 flex-col justify-center items-center gap-2 border-r border-slate-200">
                 <Image
                     src="/images/layaw_logo.png"
                     alt="Descriptive Alt Text"
                     width={350}
                     height={250}
-                    className=" object-cover w-full h-auto lg:px-16 "
+                    className=" object-cover w-full h-auto lg:px-16 z-10"
                 />
                 <p className=" text-sm text-center font-semibold text-primary uppercase opacity-80">
                     Medical Spa · Laser Hair Removal Service · Beauty, cosmetic & personal care
@@ -32,7 +32,7 @@ const Page = async () => {
                     {visits.map((vis, index) => (<Link href={vis.url} key={index} target="_blank"><div className=" p-2 rounded-full bg-amber-100 text-primary hover:brightness-95">{vis.icon}</div></Link>))}
                 </div>
             </div>
-            <div className=" lg:col-span-2 flex flex-col items-center justify-center">
+            <div className=" lg:col-span-2 flex flex-col px-10 items-center justify-center">
                 <Image
                     src="/images/layaw_logo.png"
                     alt="Descriptive Alt Text"
@@ -41,7 +41,7 @@ const Page = async () => {
                     className=" md:hidden"
                 />
                 <div className=" grid gap-2 px-5 pt-5">
-                    <p className=" font-bold md:text-xl text-amber-800 text-center">Welcome, Valued Practitioner!</p>
+                    <p className=" font-bold md:text-xl text-primary text-center">Welcome, Valued Practitioner!</p>
                     <p className=" text-xs md:text-sm text-center text-slate-700">Log in to manage your client appointments and access patient information.</p>
                 </div>
                 <LoginForm />
